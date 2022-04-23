df65669333df4a2d6edda76a492822c07e52c9c8
Pavel <parbeck17@gmail.com>
2022-04-23 16:59:45 +0300

    fix task 8

--- js/task-08.js
+++ js/task-08.js
@@ -3,15 +3,14 @@ const form = document.querySelector(".login-form");
 form.addEventListener("submit", function(element){
    element.preventDefault();
 
-   const formData = {};
-
    const inputsForm = Array.from(this.querySelectorAll("input"));
    if(inputsForm.some(element => element.value ===  "")){
       alert("Все поля обязательны для заполнения!");
    }else {
-      inputsForm.map(elem => {
-         formData[elem.name] = elem.value;
-      });
+      const formData = {
+         email: element.currentTarget.elements.email.value,
+         password: element.currentTarget.elements.password.value
+      };
       console.log(formData);
       this.reset();
    }
